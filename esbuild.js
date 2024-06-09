4ad958a6e00e2dcc61d56fde586b8e0607ccf4fa
JulianKominovic <juliankominovic@gmail.com>
2024-06-09 18:20:21 -0300

    chore: upload build folder

--- esbuild.js
+++ esbuild.js
@@ -1,14 +1,21 @@
 import esbuild from "esbuild";
 
+const isDev = process.env.NODE_ENV === "development";
+
 async function watch() {
   let ctx = await esbuild.context({
     entryPoints: ["./src/index.tsx"],
-    minify: false,
+    minify: !isDev,
     outfile: "./build/bundle.js",
     bundle: true,
     loader: { ".ts": "ts" },
     jsx: "automatic",
   });
-  await ctx.serve({ port: 5000, servedir: "./build" });
+  if (isDev) {
+    await ctx.serve({ port: 5000, servedir: "./build" });
+  } else {
+    await ctx.cancel();
+    await ctx.rebuild();
+  }
 }
 watch();
