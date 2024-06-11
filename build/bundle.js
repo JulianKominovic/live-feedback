26a1539171b2592c89a85bcd9f1b8927ce790ad0
JulianKominovic <juliankominovic@gmail.com>
2024-06-10 23:51:53 -0300

    chore: package json scripts

--- build/bundle.js
+++ build/bundle.js
@@ -3,7 +3,7 @@
  * Live Feedback script.
  * @version 1.1.0
  * @description Leave feedback on any website and share it with your team 🚀.
- * @date 2024-06-11T02:45:06.795Z
+ * @date 2024-06-11T02:51:54.786Z
  * @see https://github.com/JulianKominovic/live-feedback
  * @see https://jkominovic.dev/live-feedback
  **/
