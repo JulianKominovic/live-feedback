365751076ac54d0b37aa50f24f64f0f762a3565d
JulianKominovic <juliankominovic@gmail.com>
2024-06-12 23:47:45 -0300

    fix: stroke-width prop

--- src/ui/molecules/DeviceInfoTags.tsx
+++ src/ui/molecules/DeviceInfoTags.tsx
@@ -104,7 +104,7 @@ export default function DeviceInfoTags({
           <svg
             stroke="currentColor"
             fill="currentColor"
-            stroke-width="0"
+            strokeWidth="0"
             viewBox="0 0 256 256"
             height="1em"
             width="1em"
