ec8232228863c8d9bc9448f6c56c7115c9ce6574
JulianKominovic <juliankominovic@gmail.com>
2024-06-15 17:55:36 -0300

    chore: change auth server url

--- src/const.ts
+++ src/const.ts
@@ -1,8 +1,8 @@
 const IS_DEV = false;
 export const GH_TOKEN_COOKIE_KEY = "GH-LIVE-FEEDBACK-TOKEN";
 export const GH_AUTH_SERVER_BASE_URL = IS_DEV
-  ? "http://localhost:4000"
-  : "https://live-feedback.onrender.com";
+  ? "http://localhost:3000/api"
+  : "https://live-feedback.jkominovic.dev/api";
 export const GH_REPO = document.currentScript?.getAttribute("repo") ?? "";
 export const GH_OWNER = document.currentScript?.getAttribute("owner") ?? "";
 export const MINIMUM_CSS_SELECTORS_FOR_ELEMENT_TO_SHOW_BUBBLE = 2;
